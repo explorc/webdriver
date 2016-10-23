60435256fff422b00b81690d901a3a47d45678d2
Fredrik <explorc@gmail.com>
2016-10-23 17:35:44 +0200

    change to firefoxDriver
    Added test steps and attachments

--- src/test/java/example/NewTest.java
+++ src/test/java/example/NewTest.java
@@ -1,22 +1,48 @@
 package example;		
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.AssertJUnit;
-import org.openqa.selenium.By;		
+
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;		
-import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.Augmenter;
 import org.testng.Assert;		
-import org.testng.annotations.Test;	
+import org.testng.annotations.Test;
+
+import ru.yandex.qatools.allure.annotations.Step;
+import ru.yandex.qatools.allure.annotations.Attachment;
 import org.testng.annotations.BeforeTest;	
 import org.testng.annotations.AfterTest;		
 public class NewTest {		
-	    private WebDriver driver;		
+	    private FirefoxDriver driver;		
 		@Test				
-		public void testEasy() {	
+		public void testEasy() {
+			printing();
 			driver.get("http://www.guru99.com/selenium-tutorial.html");  
+			String title = driver.getTitle();
+			byte [] scrFile = driver.getScreenshotAs(OutputType.BYTES);
+			saveScreenshot(scrFile);
+			AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials")); 		
+		}
+		@Test				
+		public void testEasy2() {
+
+			driver.get("http://www.guru99.com/selenium-tutorial.html");
 			String title = driver.getTitle();				 
 			AssertJUnit.assertTrue(title.contains("Free Selenium Tutorials")); 		
-		}	
+		}
+		@Step("Open page.")
+		public void printing() {
+			System.out.println("Getting page!");
+		}
+		@Attachment(value = "Page screenshot", type = "image/png")
+		public byte[] saveScreenshot(byte[] screenShot) {
+		    return screenShot;
+		}
 		@BeforeTest
 		public void beforeTest() {	
 		    driver = new FirefoxDriver();  
